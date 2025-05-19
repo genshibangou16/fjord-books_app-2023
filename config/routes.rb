31ca130990a445f9457ea0a5e3a416b3671288b1
genshibangou16 <genshibangou16.rakkan@gmail.com>
2025-05-19 12:34:29 +0900

    ユーザー登録フォームのリンクを修正し、ユーザーリソースのルーティングを更新

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-    sessions: 'users/sessions',
     registrations: 'users/registrations'
   }
   resources :books
-  resources :users
+  resources :users, only: [:index, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
