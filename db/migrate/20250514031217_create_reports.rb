99c3dfaad5b4cd239b7e98812b72d85a6f9069aa
genshibangou16 <genshibangou16.rakkan@gmail.com>
2025-05-20 17:15:40 +0900

    Add null constraints to title in reports and comment fields

--- db/migrate/20250514031217_create_reports.rb
+++ db/migrate/20250514031217_create_reports.rb
@@ -2,7 +2,7 @@ class CreateReports < ActiveRecord::Migration[7.0]
   def change
     create_table :reports do |t|
       t.references :user, null: false, foreign_key: true
-      t.string :title
+      t.string :title, null: false
       t.text :memo
 
       t.timestamps
--- db/migrate/20250514074706_create_comments.rb
+++ db/migrate/20250514074706_create_comments.rb
@@ -2,7 +2,7 @@ class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
       t.references :user, null: false, foreign_key: true
-      t.text :comment
+      t.text :comment, null: false
       t.references :commentable, polymorphic: true, null: false
 
       t.timestamps
